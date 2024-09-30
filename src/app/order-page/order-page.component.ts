0b66b0526a6f4509b106307d5ec6716309dd2f9b
Hunter-Wigal <hgw00002@mix.wvu.edu>
2024-09-30 16:29:51 -0400

    Temporarily remove ability to place orders

--- src/app/order-page/order-page.component.ts
+++ src/app/order-page/order-page.component.ts
@@ -3,14 +3,23 @@ import { Title } from '@angular/platform-browser';
 import { OrderModel } from './order.model';
 import { NgForm } from '@angular/forms';
 import { FilesService } from '../files.service';
-import { cakes, cupcakePrices, icing, macaronPrices, macarons, quantities, signatureCakes, signatureMacarons } from './flavors';
+import {
+  cakes,
+  cupcakePrices,
+  icing,
+  macaronPrices,
+  macarons,
+  quantities,
+  signatureCakes,
+  signatureMacarons,
+} from './flavors';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 // import * as $ from 'jquery';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+  styleUrls: ['./order-page.component.css'],
 })
 
 //@Injectable({ providedIn: 'root' })
@@ -47,12 +56,11 @@ export class OrderPageComponent implements OnInit {
   validIcingFlavor: boolean;
 
   constructor(private titleService: Title, private fs: FilesService) {
-    this.titleService.setTitle("How to Order");
+    this.titleService.setTitle('How to Order');
     this.macaronSelected = true;
     this.cupCakeSelected = true;
     this.cakeSelected = false;
     this.customCakeSelected = true;
-
 
     this.validForm = true;
     this.macaronFlavors = macarons;
@@ -81,27 +89,25 @@ export class OrderPageComponent implements OnInit {
     this.largeScreen = window.innerWidth > 1000;
     window.onresize = () => {
       this.largeScreen = window.innerWidth > 1000;
-    }
-
+    };
   }
 
   ngOnInit(): void {
-    const radio1 = document.getElementById("Radio1");
-    const radio2 = document.getElementById("Radio2");
-    const signatureForm = document.getElementById("SignatureForm");
-    const customForm = document.getElementById("CustomForm");
+    const radio1 = document.getElementById('Radio1');
+    const radio2 = document.getElementById('Radio2');
+    const signatureForm = document.getElementById('SignatureForm');
+    const customForm = document.getElementById('CustomForm');
 
     if (radio1 && radio2 && signatureForm && customForm) {
       radio1.onclick = function () {
         signatureForm.hidden = false;
         customForm.hidden = true;
-      }
+      };
       radio2.onclick = function () {
         signatureForm.hidden = true;
         customForm.hidden = false;
-      }
-    }
-    else {
+      };
+    } else {
       console.log(radio1);
     }
   }
@@ -110,85 +116,108 @@ export class OrderPageComponent implements OnInit {
 
   select(text: string | number, element: string) {
     const button = document.getElementById(element);
-    const type = document.getElementById("type");
-    const quantity = document.getElementById("quantity");
-    const price = document.getElementById("price");
+    const type = document.getElementById('type');
+    const quantity = document.getElementById('quantity');
+    const price = document.getElementById('price');
     this.quantities = quantities;
 
     if (button) {
       button.innerText = text.toString();
     }
 
-    if (element.includes("typeButton")) {
+    if (element.includes('typeButton')) {
       if (type) {
         type.innerText = text.toString();
-        if (text.toString() === "Cupcakes") {
+        if (text.toString() === 'Cupcakes') {
           this.macaronSelected = false;
           this.cakeSelected = false;
           this.cupCakeSelected = true;
-        }
-        else if (text.toString() === "Macarons") {
+        } else if (text.toString() === 'Macarons') {
           this.macaronSelected = true;
           this.cakeSelected = false;
           this.cupCakeSelected = false;
           this.customCakeSelected = false;
           this.quantities.shift();
-        }
-        else if (text.toString() === "Cake") {
+        } else if (text.toString() === 'Cake') {
           this.macaronSelected = false;
           this.cakeSelected = true;
           this.cupCakeSelected = false;
         }
       }
-    }
-    else if (element.includes("quantityButton")) {
+    } else if (element.includes('quantityButton')) {
       if (quantity) {
         quantity.innerText = text.toString();
       }
     }
 
-
-    if (type && quantity && type.innerText !== "" && quantity.innerText !== "" && price) {
-      if (type.innerText === "Cupcakes") {
-        price.innerText = "$" + cupcakePrices[quantities.indexOf(parseInt(quantity.innerText))].toString();
-      }
-      else if (type.innerText === "Macarons") {
-        price.innerText = "$" + macaronPrices[quantities.indexOf(parseInt(quantity.innerText))].toString();
+    if (
+      type &&
+      quantity &&
+      type.innerText !== '' &&
+      quantity.innerText !== '' &&
+      price
+    ) {
+      if (type.innerText === 'Cupcakes') {
+        price.innerText =
+          '$' +
+          cupcakePrices[
+            quantities.indexOf(parseInt(quantity.innerText))
+          ].toString();
+      } else if (type.innerText === 'Macarons') {
+        price.innerText =
+          '$' +
+          macaronPrices[
+            quantities.indexOf(parseInt(quantity.innerText))
+          ].toString();
       }
     }
-  }// End of select
+  } // End of select
 
   async addOrder(form: NgForm, formType: number) {
-    let typeSelect = document.getElementById("typeButton");
-    let quantitySelect = document.getElementById("quantityButton");
-    let cakeSelect = document.getElementById("cakeButton");
-    let macaronSelect = document.getElementById("macaronButton");
-    let icingSelect = document.getElementById("icingButton");
-    let commentsSelect = document.getElementById("comments");
-    let dateSelect = document.getElementById("dateSelect");
+    let unavailable = true;
+
+    if(unavailable){
+      window.alert("Sophie is currently unavailable for orders, please check back later!");
+      return;
+    }
+
+    let typeSelect = document.getElementById('typeButton');
+    let quantitySelect = document.getElementById('quantityButton');
+    let cakeSelect = document.getElementById('cakeButton');
+    let macaronSelect = document.getElementById('macaronButton');
+    let icingSelect = document.getElementById('icingButton');
+    let commentsSelect = document.getElementById('comments');
+    let dateSelect = document.getElementById('dateSelect');
     var additionsSelect: HTMLElement | null = null;
     var valid = true;
 
     if (formType == 2) {
-      typeSelect = document.getElementById("typeButton2");
-      quantitySelect = document.getElementById("quantityButton2");
-      cakeSelect = document.getElementById("cakeButton2");
-      macaronSelect = document.getElementById("macaronButton2");
-      icingSelect = document.getElementById("icingButton");
-      commentsSelect = document.getElementById("comments2");
-      additionsSelect = document.getElementById("additionsButton");
+      typeSelect = document.getElementById('typeButton2');
+      quantitySelect = document.getElementById('quantityButton2');
+      cakeSelect = document.getElementById('cakeButton2');
+      macaronSelect = document.getElementById('macaronButton2');
+      icingSelect = document.getElementById('icingButton');
+      commentsSelect = document.getElementById('comments2');
+      additionsSelect = document.getElementById('additionsButton');
     }
 
-    let type = "";
-    let quantity = "";
-    let cake = "";
-    let icing = "";
-    let comments = "";
-    let macaron = "";
-    let additions = "";
-    let date = "";
+    let type = '';
+    let quantity = '';
+    let cake = '';
+    let icing = '';
+    let comments = '';
+    let macaron = '';
+    let additions = '';
+    let date = '';
 
-    if (typeSelect && quantitySelect && cakeSelect && commentsSelect && macaronSelect && dateSelect) {
+    if (
+      typeSelect &&
+      quantitySelect &&
+      cakeSelect &&
+      commentsSelect &&
+      macaronSelect &&
+      dateSelect
+    ) {
       type = typeSelect.innerText;
       quantity = quantitySelect.innerText;
       cake = cakeSelect.innerText;
@@ -200,63 +229,58 @@ export class OrderPageComponent implements OnInit {
         icing = icingSelect.innerText;
         additions = additionsSelect.innerText;
       }
-    }
-    else {
-      $('#errorModal').modal('show'); 
+    } else {
+      $('#errorModal').modal('show');
       return;
     }
 
-    if (type.trim() === "Select") {
+    if (type.trim() === 'Select') {
       valid = false;
       this.validType = false;
-    }
-    else {
+    } else {
       this.validType = true;
     }
-    if (quantity.trim() === "Select") {
+    if (quantity.trim() === 'Select') {
       valid = false;
       this.validQuantity = false;
-    }
-    else{
+    } else {
       this.validQuantity = true;
     }
 
     //Invalid cake/macaron flavor
-    if (cake.trim() === "Select" && (type.trim() === "Cupcakes" || type.trim() === "Cake")) {
+    if (
+      cake.trim() === 'Select' &&
+      (type.trim() === 'Cupcakes' || type.trim() === 'Cake')
+    ) {
       valid = false;
       this.validCakeFlavor = false;
-    }
-    else if (macaron.trim() === "Select" && type.trim() === "Macarons") {
+    } else if (macaron.trim() === 'Select' && type.trim() === 'Macarons') {
       valid = false;
       this.validMacaronFlavor = false;
     }
     // Valid cake/macaron flavor
-    else if(cake.trim() !== "Select"){
+    else if (cake.trim() !== 'Select') {
       this.validCakeFlavor = true;
-    }
-    else if(macaron.trim() !== "Select"){
+    } else if (macaron.trim() !== 'Select') {
       this.validMacaronFlavor = true;
     }
 
-
-    if (additions.trim() !== "Select") {
-      comments = additions.concat(", " + comments);
+    if (additions.trim() !== 'Select') {
+      comments = additions.concat(', ' + comments);
     }
 
     // Icing check
-    if (icing.trim() === "Select" && formType == 2) {
+    if (icing.trim() === 'Select' && formType == 2) {
       this.validIcingFlavor = false;
       valid = false;
-    }
-    else{ 
+    } else {
       this.validIcingFlavor = true;
     }
 
-    if(date.trim() === ""){
+    if (date.trim() === '') {
       valid = false;
       this.validDate = false;
-    }
-    else{
+    } else {
       this.validDate = true;
     }
 
@@ -266,16 +290,14 @@ export class OrderPageComponent implements OnInit {
     if (!name || name.length < 5) {
       this.validName = false;
       valid = false;
-    }
-    else {
+    } else {
       this.validName = true;
     }
 
-    if (!email || email.search("@") < 1) {
+    if (!email || email.search('@') < 1) {
       this.validEmail = false;
       valid = false;
-    }
-    else {
+    } else {
       this.validEmail = true;
     }
 
@@ -283,8 +305,19 @@ export class OrderPageComponent implements OnInit {
       return;
     }
 
-    const order = new OrderModel(form.value.name, form.value.email, form.value.phone, type, quantity, cake, macaron, icing,
-      comments, form.value.date.toString(), "Not set yet");
+    const order = new OrderModel(
+      form.value.name,
+      form.value.email,
+      form.value.phone,
+      type,
+      quantity,
+      cake,
+      macaron,
+      icing,
+      comments,
+      form.value.date.toString(),
+      'Not set yet'
+    );
 
     const success = await this.fs.addOrder(order);
 
@@ -292,11 +325,10 @@ export class OrderPageComponent implements OnInit {
       this.sendEmail(order);
 
       $('#successModal').modal('show');
-    }
-    else {
+    } else {
       $('#errorModal').modal('show');
     }
-  }//End of addOrder
+  } //End of addOrder
 
   // sendEmail() {
   //   var templateParams = {
@@ -318,30 +350,31 @@ export class OrderPageComponent implements OnInit {
       name: order.name,
       quantity: order.quantity,
       type: order.type,
-      requestedDate: order.requestedDate
-    }
+      requestedDate: order.requestedDate,
+    };
 
-    emailjs.send(this.serviceID, this.templateID, templateParams, this.publicKey)
-      .then(function (response) {
-        console.log('SUCCESS!', response.status, response.text);
-      }, function (error) {
-        console.log('FAILED...', error);
-      });
+    emailjs
+      .send(this.serviceID, this.templateID, templateParams, this.publicKey)
+      .then(
+        function (response) {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        function (error) {
+          console.log('FAILED...', error);
+        }
+      );
   }
 
-  openEmail(){
+  openEmail() {
     window.open('mailto:bakergirlsophie@gmail.com?subject=Cake Order');
   }
 
-  reload(){
+  reload() {
     window.location.reload();
   }
 
-  closeNotice(){
-    let notice = document.getElementById("notice");
-    if(notice != null)
-      notice.hidden = true;
+  closeNotice() {
+    let notice = document.getElementById('notice');
+    if (notice != null) notice.hidden = true;
   }
-
 }
-
